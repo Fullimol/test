87e6fb762b732651e9f85d1c51ea0acf6ba25309
Bruno Freijomil <bruno_freijomil@hotmail.com>
2025-05-17 01:16:50 -0300

    formato documento

--- script.js
+++ script.js
@@ -1,1 +1,5 @@
 // aca va la logica jajaj
+
+/*
+lorem ipsum dolor sit amet consectetur adipisicing elit. Temporibus a non voluptatibus ipsa culpa voluptatum neque beatae quae praesentium? Consequatur ea veritatis sequi quasi perferendis aliquid eius iusto fuga illum?
+*/
